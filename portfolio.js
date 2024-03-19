d9db49720124d323b755c83ee0f553b857911c3e
Henry Corredor González <henry.corredor7570@gmail.com>
2024-03-19 16:40:13 -0500

    section skills completed

--- portfolio.js
+++ portfolio.js
@@ -6,7 +6,6 @@ function seleccionar(link) {
     opciones[2].className = "";
     opciones[3].className = "";
     opciones[4].className = "";
-    opciones[5].className = "";
     link.className = "seleccionado";
 
     //hacemos desaparecer el menu una vez que se ha seleccionado una opcion en el modo responsive:
@@ -20,26 +19,6 @@ function responsiveMenu() {
         x.className = "responsive";
     } else {
         x.className = "";
-    }
-}
-
-//detecto el scrolling para aplicar la animación de las barras de habilidades
-window.onscroll = function() {
-    efectoHabilidades()
-};
-
-//funcion que aplica la animación de la barra de habilidades:
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("java").classList.add("barra-progreso4");
-        document.getElementById("react").classList.add("barra-progreso5");
-        document.getElementById("node").classList.add("barra-progreso6");
-        document.getElementById("springboot").classList.add("barra-progreso7");
     }
 }
 
